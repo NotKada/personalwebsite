9835c538f00beb11ec4d200ef0eb878eb8cc8eab
NotKada <notkada@gmail.com>
2026-06-01 10:44:28 +0200

    added more stuff to cookie clicker, far from finished rn

--- cookieclickerscript.js
+++ cookieclickerscript.js
@@ -9,6 +9,12 @@ let textScore = document.getElementById("score")
 let btngrandma = document.getElementById("btn-grandma");
 let textGrandmaPrice = document.getElementById("grandmaprice")
 
+let btnfactory = document.getElementById("btn-factory");
+let textFactoryPrice = document.getElementById("factoryprice")
+let factorylevel = 0
+
+let goldencookietrue = false
+
 btncookie.addEventListener("click", function() {
     cookies += clickpower;
     textScore.textContent = cookies;
@@ -24,3 +30,45 @@ btngrandma.addEventListener("click", function(){
         textGrandmaPrice.textContent = grandmaprice
     }
 })
+
+btnfactory.addEventListener("click", function(){
+    if (cookies >= factoryprice){
+        factorylevel += 1
+        cookies -= factoryprice
+        factoryprice += 10
+
+        textScore.textContent = cookies;
+        textFactoryPrice.textContent = factoryprice
+    }
+})
+
+let factoryLoop = setInterval(function(){
+    cookies += factorylevel
+    textScore.textContent = cookies;
+
+    if (Math.random() > 0.1){
+        if (goldencookietrue == false){
+            goldencookietrue = true
+            
+            const goldencookie = document.createElement("button");
+            goldencookie.textContent == "Golden Cookie"
+            goldencookie.addEventListener("click", function(){
+                clickpower += 5
+                goldencookie.remove
+                setTimeout(() => {
+                    clickpower -= 5
+                }, 50000)
+            })
+            
+            document.body.appendChild(goldencookie)
+
+            setTimeout(() => {
+                goldencookietrue == false
+                if (goldencookie){
+                    goldencookie.remove()
+                }
+            }, 75000);
+        }
+       
+    }
+}, 1000);
